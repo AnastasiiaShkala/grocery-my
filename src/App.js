0ef623f9545cd666c08ee593daa054552b458493
AnastasiiaShkala <nastyashkala@gmail.com>
2024-01-03 18:54:24 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import { ToDoList } from './ToDoList';
+
+function App() {
+  return (
+    <div>
+    <div className='header'>
+      <h1>TO DO LIST</h1>
+    </div>
+    <div>
+    <ToDoList/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ToDoList.js
@@ -0,0 +1,67 @@
+import { Component } from "react";
+import done from './done.png';
+
+export class ToDoList extends Component {
+    state = {
+        userInput: "",
+        todoList: []
+
+    }
+    onChangeEvent (e) { 
+        this.setState({userInput:e})    
+    }
+
+    addToDo (input) {
+        if (input === '') {
+            alert ("Please enter an item")
+        }
+        let listNew = this.state.todoList;
+        listNew.push(input);
+        this.setState({todoList: listNew, userInput: ""})
+    }
+    deleteItem() {
+        let listNew = this.state.todoList;
+        listNew = [];
+        this.setState({todoList: listNew})
+    }
+
+    crossedWord (event) {
+        const li = event.target;
+        li.classList.toggle('crossed');
+        }
+
+        onFormSubmit(e){
+            e.preventDefault()
+        }
+        
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.onFormSubmit}> 
+            <div className="lane">
+                <input type="text"
+                placeholder="What do you want to do today?"
+                onChange={(e) => {this.onChangeEvent(e.target.value)}}
+                value={this.state.userInput}/>
+            </div>
+            <div className="lane">
+                <button className="add" onClick={() => this.addToDo(this.state.userInput)}>ADD</button>
+            </div>
+            <div className="lane"> 
+                <ul>
+                    {this.state.todoList.map((item, index) => (
+                    <li onClick={this.crossedWord} key={index}>
+                        <img src={done} alt="icon" width="40px"/>
+                        {item}</li>
+        ))}
+                </ul>
+                </div>
+                <div className="lane">
+                <button className="delete" onClick={() => this.deleteItem()}>DELETE</button>
+                </div>
+                </form>
+            </div>
+        
+        )
+        }
+    }
